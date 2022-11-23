3a35880d238345d20831df2aefc6a3ed1fb22fa9
Evgenii Ptitsyn <eptitsyn@gmail.com>
2022-11-23 15:23:43 +0300

    sprint final fix

--- handlebars.config.js
+++ handlebars.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    data: "src/markup/data",
-    helpers: "src/markup/helpers",
-    layouts: "src/markup/layouts",
-    partials: "src/markup/partials",
+    data: 'src/markup/data',
+    helpers: 'src/markup/helpers',
+    layouts: 'src/markup/layouts',
+    partials: 'src/markup/partials',
 };
