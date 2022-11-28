6ca91243f42ff999824babc9b26e336f503740cb
Evgenii Ptitsyn <eptitsyn@gmail.com>
2022-11-28 23:11:28 +0300

    edits

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-namespace('a.b.c.d.e'); // "{"a":{"b":{"c":{"d":{"e":{}}}}}}"
-
-export default namespace;
-
-function namespace(s : string) {
-    return s.split('.').reduceRight((accumulator, currentValue) => ({ [currentValue]: accumulator }), {});
-}
