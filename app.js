cbe11893538c633ad3c4f0631a3e4d78da333ab1
Evgenii Ptitsyn <eptitsyn@gmail.com>
2022-12-02 18:33:07 +0300

    app fix

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static("dist"));
+app.get("*", function (request, response) {
+    response.sendFile(`${__dirname}/dist/index.html`);
+});
 app.listen(port, () => console.log(`Listening on port ${port}!`));
