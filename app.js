1f728c3d4fa1837dd68056983bf7b3a1852bddfa
Evgenii Ptitsyn <eptitsyn@gmail.com>
2022-12-03 00:29:26 +0300

    deploy fix

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static("./dist/"));
 
+app.use(express.static("./dist/"));
 app.get("*", function (request, response) {
     response.sendFile(`${__dirname}/dist/index.html`);
 });
